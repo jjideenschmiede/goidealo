e6d489a93082963eb94b5360aebfc346353d2f06
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-10-12 15:30:14 +0200

    Add function to check pws status codes

---
+++ pws_status_codes.go
@@ -0,0 +1,32 @@
+//********************************************************************************************************************//
+//
+// Copyright (C) 2018 - 2021 J&J Ideenschmiede GmbH <info@jj-ideenschmiede.de>
+//
+// This file is part of goidealo.
+// All code may be used. Feel free and maybe code something better.
+//
+// Author: Jonas Kwiedor (aka gowizzard)
+//
+//********************************************************************************************************************//
+
+package goidealo
+
+import (
+	"errors"
+	"strings"
+)
+
+// pwsStatusCodes
+func pwsStatusCodes(status string) error {
+
+	// Check each status codes
+	switch strings.ReplaceAll(status, " ", "") {
+	case "401":
+		return errors.New("access is not authorized")
+	case "404":
+		return errors.New("not found")
+	default:
+		return nil
+	}
+
+}
